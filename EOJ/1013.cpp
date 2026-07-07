0eecbab4d56ee79903e32310a1f99e9460d4a44e
Slev <H138082@outlook.com>
2026-07-07 17:53:20 +0800

    1013(unfinished)

---
+++ EOJ/1013.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+class BitsStr {
+private:
+    string bits;
+public:
+    
+};
+
+
+int main() {
+    int T; cin >> T;
+    for (int t; t < T; ++t) {
+        cout << "case #" << t << ": \n";
+        string num; cin >> num;
+
+    }
+    return 0;
+}
--- EOJ/mode/mode.cpp
+++ EOJ/mode/mode.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 
 int main() {
+    cout << "Hello, World!" << endl;
     return 0;
 }
