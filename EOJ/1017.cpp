8a259fcd87112b84ca7b5b0cc015111969306708
Slev <H138082@outlook.com>
2026-07-08 22:21:41 +0800

    1017

--- EOJ/1017.cpp
+++ EOJ/1017.cpp
@@ -16,48 +16,48 @@ struct BigInt {
     }
 };
 
-inline int cmp(const BigInt& a, const BigInt& b) {
-    if (a.s.size() != b.s.size())
-        return a.s.size() < b.s.size() ? -1 : 1;
-    if (a.s == b.s) return 0;
-    return a.s < b.s ? -1 : 1;
+inline BigInt operator+(const BigInt& a, const BigInt& b) {
+    string r;
+    int carry = 0, i = a.s.size() - 1, j = b.s.size() - 1;
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+        if (i >= 0) sum += a.s[i--] - '0';
+        if (j >= 0) sum += b.s[j--] - '0';
+        r += (sum % 10 + '0');
+        carry = sum / 10;
+    }
+    reverse(r.begin(), r.end());
+    return BigInt(r);
 }
 
 inline pair<BigInt, int> div_by_int(const BigInt& a, int d) {
+    bool neg = a.s[0] == '-';                    // [NEG]
+    string num = neg ? a.s.substr(1) : a.s;      // [NEG]
     string q;
     int rem = 0;
-    for (char c : a.s) {
+    for (char c : num) {
         int cur = rem * 10 + (c - '0');
         int qd = cur / d;
         if (!q.empty() || qd) q += (qd + '0');
         rem = cur % d;
     }
     if (q.empty()) q = "0";
+    if (neg && rem > 0) {                        // [NEG]
+        q = (BigInt(q) + BigInt("1")).s;          // [NEG]
+        rem = d - rem;                            // [NEG]
+    }                                             // [NEG]
+    if (neg) q = "-" + q;                         // [NEG]
     return {BigInt(q), rem};
 }
 
-/*
-string dec_to_negR(ll n, int R) {
-    if (n == 0) return "0";
-    string res;
-    while (n) {
-        int r = n % -R;
-        if (r < 0) r += -R;
-        n = (n - r) / R;
-        res += (r + '0');
-    }
-    reverse(res.begin(), res.end());
-    return res;
-}
-    */
-
-string dec_to_neg2(BigInt& n) {
+string dec_to_neg2(BigInt n) {
     if (n.s == "0") return "0";
     string res;
     while (n.s != "0") {
-        bool positive = cmp(n, BigInt("0")) > 0;
-        auto [q, r] = div_by_int(n, 2);
-        n = q;
+        auto [q, r] = div_by_int(n, 2);       // floor: r ∈ [0,1]
+        res += (r + '0');
+        string& qs = q.s;                      // n_new = (n-r)/(-2) = -q
+        n = BigInt((qs == "0") ? "0" : (qs[0] == '-' ? qs.substr(1) : "-" + qs));
     }
     reverse(res.begin(), res.end());
     return res;
