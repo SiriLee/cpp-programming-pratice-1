c4c32adc71a4e3de5ab80eab68baf8868c2ed859
Slev <H138082@outlook.com>
2026-07-09 15:11:29 +0800

    1023

--- EOJ/1023.cpp
+++ EOJ/1023.cpp
@@ -3,19 +3,42 @@
 using namespace std;
 using ll = long long;
 
-ll getCount(int n) {
-    ll rounds = (1 << n) - 1;
-    ll num = 0;
-    while (rounds--) {
-        cout <<  << endl;
+class StringGenerator {
+public:
+    StringGenerator(int n) : n_(n) {}
+    int getResults();
+private:
+    int n_;
+    vector<string> results_;
+    void dfs(const string& cur, char c);
+};
+
+int StringGenerator::getResults() {
+    dfs("", '1');
+    dfs("", '0');
+    return results_.size();
+}
+
+// no 101
+void StringGenerator::dfs(const string& cur, char c) {
+    if (cur.size() == n_ - 1) {
+        results_.push_back(cur + c);
+        return;
     }
+    string new_cur = cur + c;
+    char prev = cur.empty() ? '0' : cur.back();
+    if (prev == '0' || c == '1') {
+        dfs(new_cur, '1');
+    }
+    dfs(move(new_cur), '0');
 }
 
 int main() {
     int n;
     while (cin >> n) {
         if (n == -1) break;
-        ll count = 1 << n;
+        StringGenerator generator(n);
+        cout << generator.getResults() << endl;
     }
     return 0;
 }
