da394ab3c4f8a9dbe98dc5092d3930972108de37
Slev <H138082@outlook.com>
2026-07-09 15:47:11 +0800

    1024

---
+++ EOJ/1024.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+string parse(const string& str) {
+    string res;
+    char prev = CHAR_MAX;
+    int prev_count = 0;
+    for (char c : str) {
+        if (prev == c) {
+            ++prev_count;
+        } else if (prev < c) {
+            res.append(prev_count, prev);
+            prev_count = 1;
+        } else {
+            prev_count = 1;
+        }
+        res += c;
+        prev = c;
+    }
+    return res;
+}
+
+int main() {
+    string str; cin >> str;
+    cout << parse(str) << endl;
+    return 0;
+}
