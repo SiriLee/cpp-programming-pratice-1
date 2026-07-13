5c004d9604ac1200140e94a685fff979c3e751dd
Slev <H138082@outlook.com>
2026-07-13 12:03:25 +0800

    insert:1031

---
+++ EOJ/1033.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+// 考点: 台阶走法数 (带约束线性DP)
+// 关键: dp[i]=dp[i-1]+dp[i-2]+dp[i-3], 注意取模
+
+vector<ll> stepCount(int n) {
+    n = max(n, 3);
+    vector<ll> steps(n + 1, 0);
+    steps[0] = 1;
+    steps[1] = 1;
+    steps[2] = 2;
+    steps[3] = 4;
+    for (int i = 4; i <= n; ++i) {
+        steps[i] = steps[i - 1] + steps[i - 2] + steps[i - 3] + steps[i - 4];
+    }
+    return steps;
+}
+
+int main() {
+    int T; cin >> T;
+    vector<int> a(T);
+    int max_val = INT_MIN;
+    for (int i = 0; i < T; ++i) {
+        cin >> a[i];
+        max_val = max(max_val, a[i]);
+    }
+    auto steps = stepCount(max_val);
+    for (int i = 0; i < T; ++i) {
+        cout << "case #" << i << ":\n";
+        cout << steps[a[i]] << endl;
+    }
+    return 0;
+}
